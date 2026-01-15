0584b861ae91a08c53757b07789bde14e6ad5384
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-01-15 20:35:39 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,37 @@
 import { tiktok, tiktokSlide } from './dl/tiktok';
 import { igdl, fbdl } from './dl/social';
 import { twitter } from './dl/twitter';
+import { pinterest } from './dl/pinterest';
 import { search as ytSearch, ytmp3, ytmp4, play } from './dl/youtube';
 import { ssweb, remini, styleText, wattpad, chords, morse, tts } from './utils/tools';
 import { ephoto } from './utils/maker';
+import { lyrics } from './search/lyrics';
 
 export * from './types';
+
 export { 
   tiktok, 
   tiktokSlide, 
   igdl, 
-  fbdl,
+  fbdl, 
   twitter,
   twitter as xdl,
+  pinterest,
   ytSearch, 
   ytmp3, 
   ytmp4, 
   play,
-  ssweb,
-  tts,
+  ssweb, 
   remini, 
   styleText, 
   wattpad, 
   chords,
   morse,
-  ephoto
+  tts,
+  ephoto,
+  lyrics
 };
+
 export const downloader = {
   tiktok,
   tiktokSlide,
@@ -33,15 +39,19 @@ export const downloader = {
   fbdl,
   twitter,
   xdl: twitter,
+  pinterest,
   ytmp3,
   ytmp4,
   play
 };
+
 export const search = {
   youtube: ytSearch,
   wattpad,
-  chords
+  chords,
+  lyrics
 };
+
 export const tools = {
   ssweb,
   remini,
@@ -50,6 +60,7 @@ export const tools = {
   tts,
   ephoto
 };
+
 export default {
   downloader,
   search,
