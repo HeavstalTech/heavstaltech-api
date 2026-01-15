6e55597a78fd46b0b2dda115db780f74cc5f7bd2
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-01-15 21:17:28 +0100

    Update test.js

--- test.js
+++ test.js
@@ -50,13 +50,16 @@ async function main() {
   results.push(await runTest("Search: Lyrics (Genius)", async () => {
     try {
         const res = await api.search.lyrics("Rema fun");
+        console.log("\n📦 DATA RECEIVED:");
+        console.log(JSON.stringify(res, null, 2));
+        console.log("-----------------------------------");
         return res.status && res.lyrics.length > 0;
     } catch (e) {
         console.log(`   (⚠️ Lyrics Error: ${e.message})`);
         return true; 
     }
   }));
-
+  
   results.push(await runTest("Twitter: Downloader", async () => {
     try {
         const res = await api.downloader.twitter("https://x.com/elonmusk/status/2009777814459781422?s=20"); 
